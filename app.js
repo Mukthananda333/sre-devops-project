996ca2eb008c4f1822d62f8811702ba5c6815879
Muktha Nanda Korimi <mukthananda444@gmail.com>
2026-05-26 08:52:54 +0000

    Updated app.js message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('FinacPlus DevOps Assignment Successfully Running');
+    res.send('FinacPlus DevOps Assignment updated Successfully Running');
 });
 
 app.listen(3000, () => {
